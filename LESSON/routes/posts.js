d26fb67cb5d5d0a8d289cf1c696e3c146dee514a
LookeiCode <137450026+LookeiCode@users.noreply.github.com>
2023-10-29 02:58:42 -0400

    Made first GET request + Prisma table join

--- LESSON/routes/posts.js
+++ LESSON/routes/posts.js
@@ -3,6 +3,27 @@ const router = express.Router();
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
+
+router.get('/', async (req, res) => {
+
+    try {
+        const blogPosts = await prisma.post.findMany({
+            include: {
+                author: true
+            }
+        })
+        res.json(blogPosts);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error: 'Something went wrong getting blog posts'});
+    }
+})
+
+// L10-13 basically joins the author table into the posts table when you make a get request
+// Returns all the posts that exist + if you include (L11) "author: true" it'll bring information about the author of that post in the request
+
+// Notice how on L10 it has prisma.POST - that's how it's referencing the post table + findMany is finding all the posts associated with the post table
+
 router.post('/new', async (req, res) => {
     try {
         const {title, content, authorId} = req.body;
@@ -18,12 +39,12 @@ router.post('/new', async (req, res) => {
         return res.json(newPost);
     } catch (error) {
         console.log(error);
-        res.status(500).json({error: 'Something went wrong'});
+        res.status(500).json({error: 'Something went wrong making a post'});
     }
 });
 
 // You'll notice that we're using a bunch of objects. Not important, but just so you know what you're working with
-// Each column is also considered a "field" - like you there's fields to input information/values
+// Each column is also considered a "field" - like there's fields to input information/values
 
 // L8 connects whatever you write in the body in Postman to the columns specified
 // L9 creates a new post table (with the data from L8)
